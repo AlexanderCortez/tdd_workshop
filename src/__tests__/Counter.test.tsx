056ed247cdc023fced8e3f2e6befb03895b00ed8
Alexander Cortez <jcortez@applaudostudios.com>
2021-04-15 14:04:32 -0600

    ✅ Decrement test

--- src/__tests__/Counter.test.tsx
+++ src/__tests__/Counter.test.tsx
@@ -6,7 +6,7 @@ describe('Counter Test', (): void => {
     const wrapper = shallow(<Counter />);
     expect(wrapper.hasClass('counter__wrapper')).toBeTruthy();
     expect(wrapper.find('.counter__heading')).toHaveLength(1);
-    expect(wrapper.find('.counter__button')).toHaveLength(1);
+    expect(wrapper.find('.counter__button')).toHaveLength(2);
   });
 
   it('should display intial value', () => {
@@ -16,7 +16,29 @@ describe('Counter Test', (): void => {
 
   it('should increase 1', () => {
     const wrapper = mount(<Counter />);
-    wrapper.find('.counter__button').simulate('click');
+    wrapper.find('.counter__button').at(1).simulate('click');
+    expect(wrapper.find('.counter__heading').text()).toEqual('1');
+  });
+
+  it('should display decrement button', () => {
+    const wrapper = shallow(<Counter />);
+    expect(wrapper.find('.counter__button.decrement')).toHaveLength(1);
+    expect(wrapper.find('.counter__button.decrement').text()).toEqual(
+      'Decrement'
+    );
+  });
+
+  it('should keep 0 as minimun value on "decrement"', () => {
+    const wrapper = shallow(<Counter />);
+    wrapper.find('.counter__button').at(0).simulate('click');
+    expect(wrapper.find('.counter__heading').text()).toEqual('0');
+  });
+
+  it('should decrement 1', () => {
+    const wrapper = shallow(<Counter />);
+    wrapper.find('.counter__button').at(1).simulate('click');
+    wrapper.find('.counter__button').at(1).simulate('click');
+    wrapper.find('.counter__button').at(0).simulate('click');
     expect(wrapper.find('.counter__heading').text()).toEqual('1');
   });
 });
--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -3,14 +3,31 @@ import React, { useState } from 'react';
 const Counter = (): React.ReactElement => {
   const [count, setCount] = useState(0);
 
-  const onChange = (): void => {
-    setCount(count + 1);
+  const universalHandler = (increment = true): void => {
+    let newCount = count;
+    if (increment) {
+      newCount += 1;
+    }
+    if (!increment && newCount > 0) {
+      newCount -= 1;
+    }
+    setCount(newCount);
   };
 
   return (
     <div className="counter__wrapper">
       <span className="counter__heading">{count}</span>
-      <button onClick={onChange} type="button" className="counter__button">
+      <button
+        onClick={(): void => universalHandler(false)}
+        className="counter__button decrement"
+      >
+        Decrement
+      </button>
+      <button
+        onClick={(): void => universalHandler()}
+        type="button"
+        className="counter__button"
+      >
         Increment
       </button>
     </div>
